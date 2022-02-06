d9359095774fe4dff75739f13ccae4ec8e80a53c
Savron <savronbek098@gmail.com>
2022-02-06 12:02:29 +0500

    api

---
+++ store/serializers.py
@@ -0,0 +1,26 @@
+from gc import collect
+from rest_framework import serializers
+from decimal import Decimal
+from .models import Product, Collection
+
+
+class CollectionSerializer(serializers.ModelSerializer):
+
+
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
+
+
+
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+
+    price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+
+    def calculate_tax(self, product: Product):
+        return product.unit_price * Decimal(1.1)
---
+++ store/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Product
+from .serializers import ProductSerializer
+from store import serializers
+
+@api_view(['GET', 'POST'])
+def product_list(request):
+    if request.method == 'GET':
+        queryset = Product.objects.select_related('collection').all()
+        serializer = ProductSerializer(queryset, many=True, context={'request': request})
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        # serializer.validated_data
+        return Response('ok')
+
+
+
+@api_view()
+def product_detail(request, id):
+
+    product = get_object_or_404(Product, pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+@api_view()
+def collection_detail(request, pk):
+    return Response('ok')
